f9a4033accbd172f07952028e8f78380d90c85d5
Atul yadav <57094600+atul2501@users.noreply.github.com>
2025-03-07 15:41:42 +0530

    Create P4.py

---
+++ P4.py
@@ -0,0 +1,28 @@
+#P4.py
+#Arithmetic: Input some numbers, do some simple arithmetic, output results.
+
+num1=int(input("enter your first number : "))
+num2=int(input("enter your second number : "))
+
+sum=num1+num2
+sub=num1-num2
+product=num1*num2
+quotient = num1 // num2
+remainder = num1 % num2
+
+print(f"sum of {num1} and {num2} is : '{sum}'")
+print(f"subtracting of {num1} and {num2} is :'{sub}'")
+print(f"the product of the {num1} and the {num2} is :'{product}'")
+print(f"quotient of :'{quotient}'' & the remainder :'{remainder}' ")
+
+
+#o/p
+'''
+(base) atul2501@Atuls-MacBook-Air Python Practice project list % python P4.py
+enter your first number : 23
+enter your second number : 12
+sum of 23 and 12 is : '35'
+subtracting of 23 and 12 is :'11'
+the product of the 23 and the 12 is :'276'
+quotient of :'1'' & the remainder :'11' 
+'''
